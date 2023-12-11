a86fa52ebbc8c7ab08033c116a9b8cb73507a4b1
Bender's Development <134189873+BenderLender@users.noreply.github.com>
2023-12-10 22:34:28 -0600

    Add files via upload

---
+++ config/cloackroom.lua
@@ -0,0 +1,6 @@
+skins = {
+	[0] = { 
+		{name="Police Officer", model="s_m_y_cop_01"},
+	    {name="Sheriff Male", model="s_m_y_sheriff_01"}
+	}
+}
---
+++ config/vehicles.lua
@@ -0,0 +1,5 @@
+vehicles = {
+	[0] = {
+	   {name="Park Ranger Truck", model="pranger"},
+	}
+}
